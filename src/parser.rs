7bb32a73b9a4a6e82124a4ec051e605f9fc21d41
Matthew Kuo <matthewkuo92@gmail.com>
2016-03-12 17:46:24 -0800

    parse from ngram txt

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,28 @@
 use std::io;
-use std::io::BufRead;
+use std::io::{BufRead, Error, ErrorKind};
 use std::fs::File;
+
+use std::collections::HashMap;
 
 pub fn read_ngram(path: &str) -> Result<(), io::Error> {
     let file = try!(File::open(path));
     let reader = io::BufReader::new(&file);
 
+    // Mapping of n-gram to log probability
+    let mut map: HashMap<String, i64> = HashMap::new();
+    let mut total = 0;
+
     for line in reader.lines() {
-        let text = line.unwrap().split(' ').collect();
-        println!("{}", l);
+        let line: String = line.unwrap();
+        let collect_text: Vec<&str> = line.split(' ').collect();
+        let text: String = collect_text[1].into();
+        let amount: i64 = match text.parse() {
+            Ok(result) => result,
+            Err(_) => return Err(Error::new(ErrorKind::InvalidData, "Not an int"))
+        };
+
+        total += amount;
+        map.insert(collect_text[0].into(), amount);
     }
 
     Ok(())
