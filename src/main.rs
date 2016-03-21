acb0988f4d8ef6012b0a3d7a66aeb6f9c8807ccd
Matthew Kuo <matthewkuo92@gmail.com>
2016-03-21 00:15:55 -0700

    playfair deciphering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod scoring;
+mod playfair;
+
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
+    let mut key = [['a', 'b', 'c', 'd', 'e'],
+                    ['f', 'g', 'h', 'i', 'k'],
+                    ['l', 'm', 'n', 'o', 'p'],
+                    ['q', 'r', 's', 't', 'u'],
+                    ['v', 'w', 'x', 'y', 'z']];
+
+    let map: HashMap<String, f64> = match scoring::read_ngram("./ngrams/quadgrams.txt") {
+        Ok(m) => m,
+        Err(err) => panic!("Failed to parse ngram file: {:?}", err)
+    };
+
 }
